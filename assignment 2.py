429377f36693b93a026893fdbf6607a84e85b259
katapet101 <158121840+katapet101@users.noreply.github.com>
2024-02-26 14:51:24 -0500

    Assignment  2-2

---
+++ assignment 2.py
@@ -0,0 +1,8 @@
+currentYear = 2024
+futureYear = 2050
+myCurrentAge = 19
+myNewAge = (futureYear-currentYear) + myCurrentAge
+
+
+print("My current age is " + str(myCurrentAge))
+print("My age in 2050 will be " + str(myNewAge))
