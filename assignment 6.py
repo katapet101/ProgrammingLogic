d65a021ba29a651d40e8f47fb18fcf304b2fc2e2
katapet101 <158121840+katapet101@users.noreply.github.com>
2024-03-04 14:15:09 -0500

    Update assignment 6.py

--- assignment 6.py
+++ assignment 6.py
@@ -5,34 +5,21 @@ minDay = 1
 maxDay = 31
 validDate = True
 
-year = input('Enter Year')
-day = input('Enter Day')
-month = input('Enter Month') 
+
+month = input('Enter Month: ') 
+day = input('Enter Day: ')
+year = input('Enter Year: ')
 
 
-if year < minYear:
-    validDate = False 
-elif year > minYear:
-    validDate = True 
-elif year < minMonth:
+if int(year) < minYear:
     validDate = False
-elif month >= minMonth:
-    validDate = True
-elif month > maxMonth:
+elif int(month) < minMonth or int(month) > maxMonth:
     validDate = False
-elif month <= maxMonth:
-    validDate = True
-elif day < minDay:
+elif int(day) < minDay or int(day) > maxDay:
     validDate = False
-elif day >= minDay:
-    validDate = True
-elif day > maxDay:
-    validDate = False
-elif day <= maxDay:
-    validDate = True
 
 
 if validDate == True:
-    print(str(month) + '/' +str(day) + '/' + str(year) + 'is a valid date')
+    print(str(month) + '/' +str(day) + '/' + str(year) + ' is a valid date')
 if validDate == False:
-    print(str(month) + '/' +str(day) + '/' + str(year) + 'is not a valid date')
+    print(str(month) + '/' +str(day) + '/' + str(year) + ' is not a valid date')
