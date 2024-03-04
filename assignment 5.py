b08f7d46e0028f3f8d3e3d8400143cd267c1c520
katapet101 <158121840+katapet101@users.noreply.github.com>
2024-03-04 13:24:53 -0500

    Assignment 3-2

---
+++ assignment 5.py
@@ -0,0 +1,23 @@
+testScore = 87
+classRank = 60
+
+if testScore >= 90:
+    if classRank >= 25:
+        print("Accept")
+    else:
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
+        
